c5667d0a3cffc4683d4564f93053c9bdc130260f
cparros <celparros@gmail.com>
2021-02-04 22:07:20 -0500

    added write file .then

--- index.js
+++ index.js
@@ -4,17 +4,46 @@ const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
 inquirer.prompt([
   {
-  type: 'input',
-  input: 'What is the title of your project',
-  name: 'project'
-  }
-])
+    type: 'input',
+    input: 'What is the title of your project?',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    input: 'Could you give a description of your project?',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    input: 'What was the motivation behind your project?',
+    name: 'motivation'
+  },
+  {
+    type: 'input',
+    input: 'What problem does your project solve?',
+    name: 'problem'
+  },
+  {
+    type: 'input',
+    input: 'What did you learn while completing this project?',
+    name: 'learn'
+  },
+  {
+    type: 'input',
+    input: 'What is your future plan for this project?',
+    name: 'future'
+    },
+]).then(response => {
+  const readME = `
+  
+  `
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+})
 
-// TODO: Create a function to initialize app
-function init() {}
+fs.writeFile('README.md', readME, (err) => 
+err? console.error(err) : console.log('Success!')
+)
 
-// Function call to initialize app
-init();
+
+
+
